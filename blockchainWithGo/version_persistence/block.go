dc8541b5f260c9086c88918a25ec72428409a289
AaronChen <chendai2004@hotmail.com>
2017-12-21 23:18:35 +0800

    modify

---
+++ blockchainWithGo/version_persistence/block.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+	"time"
+)
+
+type Block struct {
+	Timestamp      int64
+	Data           []byte
+	PrevBlockchain []byte
+	Hash           []byte
+	Nonce          int
+}
+
+//将Block序列化为一个字节数组
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
+}
+
+//将字节数组反序列化为一个Block
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &block
+}
+
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{
+		Timestamp:      time.Now().Unix(),
+		Data:           []byte(data),
+		PrevBlockchain: prevBlockHash,
+		Hash:           []byte{},
+		Nonce:          0,
+	}
+
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
+	return block
+}
+
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
