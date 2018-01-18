c94e16cab93c2d390b0e5462dd701682da5964f1
AaronChen <chendai2004@hotmail.com>
2018-01-18 22:22:37 +0800

    update

--- blockchainWithGo/transaction/src/proofofwork.go
+++ blockchainWithGo/transaction/src/proofofwork.go
@@ -27,3 +27,42 @@ func NewProofOfWork(b *Block) *ProofOfWork{
 
 	return pow 
 }
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.HashTrasactions(),
+			IntToHex(pow.block.Timestamp),
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+
+	return data 
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int 
+	var hash [32]byte 
+	nonce :=0
+
+	fmt.Printf("Mining a new block")
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+
+		hash = sha256.Sum256(data)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			fmt.Printf("\r%x", hash)
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Print("\n\n")
+
+	return nonce, hash[:]
+}
